04c3666af483fd85e5346ddebfd3a8e8dd4eaea3
Vishal Mishra <vishal.k.mishra2@gmail.com>
2024-07-28 22:06:42 +0530

    Bug fix

--- run_scripts/github_download.py
+++ run_scripts/github_download.py
@@ -1,11 +1,10 @@
-from stocks import perf_analyzer
 from stocks.resource import StocksDataAPI
 from stocks.safe_stocks import SafeStocks
 from stocks.technical_analysis import StockDataAnalysis
 
 StocksDataAPI().download_data()
 
-task_list = [StockDataAnalysis.compute_and_save, SafeStocks.save, perf_analyzer.save_loosers_portfolio]
+task_list = [StockDataAnalysis.compute_and_save, SafeStocks.save]
 
 for task in task_list:
     try:
